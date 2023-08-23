17afa10a81a6a95ef1b63f658fb0aed6abe469f3
kanishkasubash <kanishkasubash@gmail.com>
2023-08-23 19:00:00 +0530

    modified:   nike-website-tailwind-react/src/App.jsx 	new file:   nike-website-tailwind-react/src/Components/Nav.jsx 	new file:   nike-website-tailwind-react/src/Sections/CustomerReviews.jsx 	new file:   nike-website-tailwind-react/src/Sections/Footer.jsx 	new file:   nike-website-tailwind-react/src/Sections/Hero.jsx 	new file:   nike-website-tailwind-react/src/Sections/PopularProducts.jsx 	new file:   nike-website-tailwind-react/src/Sections/Services.jsx 	new file:   nike-website-tailwind-react/src/Sections/SpecialOffers.jsx 	new file:   nike-website-tailwind-react/src/Sections/Subscribe.jsx 	new file:   nike-website-tailwind-react/src/Sections/SuperQuality.jsx 	new file:   nike-website-tailwind-react/src/Sections/index.js 	modified:   nike-website-tailwind-react/vite.config.js

--- nike-website-tailwind-react/src/App.jsx
+++ nike-website-tailwind-react/src/App.jsx
@@ -1,24 +1,34 @@
-function App() {
-  return (
-    <main className="relative">
-      <Nav></Nav>
-      <section className="xl:padding-1 wide:padding-r padding-b">
-        <Hero></Hero>
-      </section>
-      <section className="padding">
-        <PopularProducts></PopularProducts>
-      </section>
-      <section className="padding">
-        <SuperQuality></SuperQuality>
-      </section>
-      <section className="py-10 padding-x">
-        <Services></Services>
-      </section>
-      <section className="padding">
-        <SpecialOffer></SpecialOffer>
-      </section>
-    </main>
-  )
-}
+import {Hero, PopularProducts, SuperQuality, Services, SpecialOffers, CustomerReviews, Subscribe, Footer} from './Sections'
+import Nav from './Components/Nav';
+
+const App = () => (
+  <main className="relative">
+    <Nav/>
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero/>
+    </section>
+    <section className="padding">
+      <PopularProducts/>
+    </section>
+    <section className="padding">
+      <SuperQuality/>
+    </section>
+    <section className="py-10 padding-x">
+      <Services/>
+    </section>
+    <section className="padding">
+      <SpecialOffers/>
+    </section>
+    <section className="padding bg-pale-blue">
+      <CustomerReviews/>
+    </section>
+    <section className="w-full py-16 padding-x sm:py-32">
+      <Subscribe/>
+    </section>
+    <section className="pb-8 bg-black padding paddiing-x padding-t">
+      <Footer/>
+    </section>
+  </main>
+);
 
 export default App
