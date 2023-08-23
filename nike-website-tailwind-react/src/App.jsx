70db9a665b78ac75dbbeaf3aaa816bcc5e2d92a9
kanishkasubash <kanishkasubash@gmail.com>
2023-08-23 17:20:09 +0530

    modified:   nike-website-tailwind-react/.eslintrc.cjs 	modified:   nike-website-tailwind-react/index.html 	modified:   nike-website-tailwind-react/package-lock.json 	modified:   nike-website-tailwind-react/package.json 	new file:   nike-website-tailwind-react/postcss.config.js 	new file:   nike-website-tailwind-react/public/favicon.ico 	deleted:    nike-website-tailwind-react/public/vite.svg 	deleted:    nike-website-tailwind-react/src/App.css 	modified:   nike-website-tailwind-react/src/App.jsx 	new file:   nike-website-tailwind-react/src/assets/icons/arrow-right.svg 	new file:   nike-website-tailwind-react/src/assets/icons/chevron-left.svg 	new file:   nike-website-tailwind-react/src/assets/icons/chevron-right.svg 	new file:   nike-website-tailwind-react/src/assets/icons/copyright-sign.svg 	new file:   nike-website-tailwind-react/src/assets/icons/facebook.svg 	new file:   nike-website-tailwind-react/src/assets/icons/hamburger.svg 	new file:   nike-website-tailwind-react/src/assets/icons/index.js 	new file:   nike-website-tailwind-react/src/assets/icons/instagram.svg 	new file:   nike-website-tailwind-react/src/assets/icons/shield-tick.svg 	new file:   nike-website-tailwind-react/src/assets/icons/star.svg 	new file:   nike-website-tailwind-react/src/assets/icons/support.svg 	new file:   nike-website-tailwind-react/src/assets/icons/truck-fast.svg 	new file:   nike-website-tailwind-react/src/assets/icons/twitter.svg 	new file:   nike-website-tailwind-react/src/assets/images/big-shoe1.png 	new file:   nike-website-tailwind-react/src/assets/images/big-shoe2.png 	new file:   nike-website-tailwind-react/src/assets/images/big-shoe3.png 	new file:   nike-website-tailwind-react/src/assets/images/collection-background.svg 	new file:   nike-website-tailwind-react/src/assets/images/customer1.jpeg 	new file:   nike-website-tailwind-react/src/assets/images/customer2.svg 	new file:   nike-website-tailwind-react/src/assets/images/footer-logo.svg 	new file:   nike-website-tailwind-react/src/assets/images/header-logo.svg 	new file:   nike-website-tailwind-react/src/assets/images/index.js 	new file:   nike-website-tailwind-react/src/assets/images/offer.svg 	new file:   nike-website-tailwind-react/src/assets/images/shoe4.svg 	new file:   nike-website-tailwind-react/src/assets/images/shoe5.svg 	new file:   nike-website-tailwind-react/src/assets/images/shoe6.svg 	new file:   nike-website-tailwind-react/src/assets/images/shoe7.svg 	new file:   nike-website-tailwind-react/src/assets/images/shoe8.svg 	new file:   nike-website-tailwind-react/src/assets/images/thumbnail-background.svg 	new file:   nike-website-tailwind-react/src/assets/images/thumbnail-shoe1.svg 	new file:   nike-website-tailwind-react/src/assets/images/thumbnail-shoe2.svg 	new file:   nike-website-tailwind-react/src/assets/images/thumbnail-shoe3.svg 	deleted:    nike-website-tailwind-react/src/assets/react.svg 	modified:   nike-website-tailwind-react/src/index.css 	modified:   nike-website-tailwind-react/src/main.jsx 	new file:   nike-website-tailwind-react/tailwind.config.js 	modified:   nike-website-tailwind-react/vite.config.js

--- nike-website-tailwind-react/src/App.jsx
+++ nike-website-tailwind-react/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>React App</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          This is My First React App
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="relative">
+      <Nav></Nav>
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero></Hero>
+      </section>
+      <section className="padding">
+        <PopularProducts></PopularProducts>
+      </section>
+      <section className="padding">
+        <SuperQuality></SuperQuality>
+      </section>
+      <section className="py-10 padding-x">
+        <Services></Services>
+      </section>
+      <section className="padding">
+        <SpecialOffer></SpecialOffer>
+      </section>
+    </main>
   )
 }
 
--- nike-website-tailwind-react/src/main.jsx
+++ nike-website-tailwind-react/src/main.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import '@fontsource-variable/outfit';
-import '@fontsource-variable/inter';
+import '@fontsource-variable/montserrat';
+import '@fontsource/palanquin/100.css';
+import '@fontsource/palanquin/200.css';
+import '@fontsource/palanquin/300.css';
+import '@fontsource/palanquin/400.css';
+import '@fontsource/palanquin/500.css';
+import '@fontsource/palanquin/600.css';
+import '@fontsource/palanquin/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
