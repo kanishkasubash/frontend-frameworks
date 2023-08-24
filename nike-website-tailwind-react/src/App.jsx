4cab55ed2553cf7a2214ba7ed647cb3a85c9be6e
kanishkasubash <kanishkasubash@gmail.com>
2023-08-24 15:50:44 +0530

    modified:   nike-website-tailwind-react/src/App.jsx 	modified:   nike-website-tailwind-react/src/Components/ShoeCard.jsx 	new file:   nike-website-tailwind-react/src/Components/index.js 	modified:   nike-website-tailwind-react/src/Sections/Hero.jsx

--- nike-website-tailwind-react/src/App.jsx
+++ nike-website-tailwind-react/src/App.jsx
@@ -1,32 +1,41 @@
-import {Hero, PopularProducts, SuperQuality, Services, SpecialOffers, CustomerReviews, Subscribe, Footer} from './Sections'
-import Nav from './Components/Nav';
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffers,
+  CustomerReviews,
+  Subscribe,
+  Footer
+} from './Sections'
+import { Nav } from './Components';
 
 const App = () => (
   <main className="relative">
-    <Nav/>
-    <section className="xl:padding-1 wide:padding-r padding-b">
-      <Hero/>
+    <Nav />
+    <section className="xl:padding-l wide:padding-r padding-b">
+      <Hero />
     </section>
     <section className="padding">
-      <PopularProducts/>
+      <PopularProducts />
     </section>
     <section className="padding">
-      <SuperQuality/>
+      <SuperQuality />
     </section>
     <section className="py-10 padding-x">
-      <Services/>
+      <Services />
     </section>
     <section className="padding">
-      <SpecialOffers/>
+      <SpecialOffers />
     </section>
     <section className="padding bg-pale-blue">
-      <CustomerReviews/>
+      <CustomerReviews />
     </section>
     <section className="w-full py-16 padding-x sm:py-32">
-      <Subscribe/>
+      <Subscribe />
     </section>
     <section className="pb-8 bg-black padding paddiing-x padding-t">
-      <Footer/>
+      <Footer />
     </section>
   </main>
 );
