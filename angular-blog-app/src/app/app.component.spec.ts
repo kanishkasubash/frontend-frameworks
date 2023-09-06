5b94097dc2aaf888bbde3be39c35406037f86b34
kanishkasubash <kanishkasubash@gmail.com>
2023-09-06 23:16:22 +0530

    renamed:    angular-demo/.editorconfig -> angular-blog-app/.editorconfig 	renamed:    angular-demo/.gitignore -> angular-blog-app/.gitignore 	renamed:    angular-demo/.vscode/extensions.json -> angular-blog-app/.vscode/extensions.json 	renamed:    angular-demo/.vscode/launch.json -> angular-blog-app/.vscode/launch.json 	renamed:    angular-demo/.vscode/tasks.json -> angular-blog-app/.vscode/tasks.json 	renamed:    angular-demo/README.md -> angular-blog-app/README.md 	renamed:    angular-demo/angular.json -> angular-blog-app/angular.json 	renamed:    angular-demo/package-lock.json -> angular-blog-app/package-lock.json 	renamed:    angular-demo/package.json -> angular-blog-app/package.json 	renamed:    angular-demo/src/app/app-routing.module.ts -> angular-blog-app/src/app/app-routing.module.ts 	renamed:    angular-demo/src/app/app.component.scss -> angular-blog-app/src/app/app.component.css 	new file:   angular-blog-app/src/app/app.component.html 	renamed:    angular-demo/src/app/app.component.spec.ts -> angular-blog-app/src/app/app.component.spec.ts 	renamed:    angular-demo/src/app/app.component.ts -> angular-blog-app/src/app/app.component.ts 	renamed:    angular-demo/src/app/app.module.ts -> angular-blog-app/src/app/app.module.ts 	renamed:    angular-demo/src/assets/.gitkeep -> angular-blog-app/src/assets/.gitkeep 	renamed:    angular-demo/src/favicon.ico -> angular-blog-app/src/favicon.ico 	renamed:    angular-demo/src/index.html -> angular-blog-app/src/index.html 	renamed:    angular-demo/src/main.ts -> angular-blog-app/src/main.ts 	new file:   angular-blog-app/src/styles.css 	renamed:    angular-demo/tsconfig.app.json -> angular-blog-app/tsconfig.app.json 	renamed:    angular-demo/tsconfig.json -> angular-blog-app/tsconfig.json 	renamed:    angular-demo/tsconfig.spec.json -> angular-blog-app/tsconfig.spec.json 	deleted:    angular-demo/src/app/app.component.html 	deleted:    angular-demo/src/styles.scss

--- angular-blog-app/src/app/app.component.spec.ts
+++ angular-blog-app/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-demo'`, () => {
+  it(`should have as title 'angular-blog-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-demo');
+    expect(app.title).toEqual('angular-blog-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-demo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-blog-app app is running!');
   });
 });
