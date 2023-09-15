e19c66c09a5e6e884fef20156d3da49f32865390
kanishkasubash <kanishkasubash@gmail.com>
2023-09-15 10:22:11 +0530

    modified:   angular-uom/src/app/Components/products/products.component.ts

--- angular-uom/src/app/Components/products/products.component.ts
+++ angular-uom/src/app/Components/products/products.component.ts
@@ -11,45 +11,44 @@ export class ProductsComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public rowIndex! : number;
+  public rowIndex!: number;
 
   public products = [{
-    'productId' : "001",
-    'productName' : "White Basmathi Rice",
+    'productId': "001",
+    'productName': "White Basmathi Rice",
     'createdDate': "Jan 29, 2020",
     'quantity': 100,
     'unitPrice': 400,
-    'productDescription':"White Basmathi Rice imported from pakistan"
+    'productDescription': "White Basmathi Rice imported from pakistan"
   },
-  { 
-  'productId' : "002",
-  'productName' : "Flour",
-  'createdDate': "Jan 29, 2020",
-  'quantity': 50,
-  'unitPrice': 190,
-  'productDescription':"Super Fine Whole grain general Purpose flour"
+  {
+    'productId': "002",
+    'productName': "Flour",
+    'createdDate': "Jan 29, 2020",
+    'quantity': 50,
+    'unitPrice': 190,
+    'productDescription': "Super Fine Whole grain general Purpose flour"
   },
-  
-  { 
-    'productId' : "003",
-    'productName' : "sugar",
+  {
+    'productId': "003",
+    'productName': "sugar",
     'createdDate': "Jan 29, 2020",
     'quantity': 1200,
     'unitPrice': 200,
-    'productDescription':"White sugar manufactured by Palwatte Factory"
-  },{
-    'productId' : "004",
-    'productName' : "Dhal",
+    'productDescription': "White sugar manufactured by Palwatte Factory"
+  },
+  {
+    'productId': "004",
+    'productName': "Dhal",
     'createdDate': "Jan 29, 2020",
     'quantity': 10,
     'unitPrice': 200,
-    'productDescription':"Imported mysoor dhal from India"
+    'productDescription': "Imported mysoor dhal from India"
   }
-  
   ]
 
-  public selectProduct(selectedRow: number){
+  public selectProduct(selectedRow: number) {
     this.rowIndex = selectedRow;
   }
 
-  }
+}
